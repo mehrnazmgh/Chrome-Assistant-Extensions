3e3b0a70027e950fe58c199837b5e4e17c946d46
mehrnaz <mehrnazmgh@gmail.com>
2024-03-02 10:52:09 +0330

    add current date and time

--- popup.js
+++ popup.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var dateElement = document.getElementById("date");
+  var timeElement = document.getElementById("time");
+
+  // Get current date
+  var currentDate = new Date();
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  var formattedDate = currentDate.toLocaleDateString("en-US", options);
+  dateElement.textContent = formattedDate;
+
+  // Get current time
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  var formattedTime =
+    padZero(hours) + ":" + padZero(minutes) + ":" + padZero(seconds);
+  timeElement.textContent = formattedTime;
+});
+
+function padZero(num) {
+  return (num < 10 ? "0" : "") + num;
+}
+
 console.log("This is a popup!");
