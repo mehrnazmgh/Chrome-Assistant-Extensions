d9dbfb41c89ecb2370aebc50f284ef538cdb5cc5
mehrnaz <mehrnazmgh@gmail.com>
2024-03-03 12:14:34 +0330

    add jalali date and time

--- popup.js
+++ popup.js
@@ -1,29 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var dateElement = document.getElementById("date");
-  var timeElement = document.getElementById("time");
+function dateTime() {
+  let currentTime = new Date();
+  let options = { year: "numeric", month: "long", day: "numeric" };
+  let currentUTCTime = currentTime.toLocaleDateString("fa-IR", options);
+  // .toLocaleDateString('fa-IR')
+  // .toUTCString)()
+  return currentUTCTime;
+}
+function clock() {
+  var date = new Date();
+  var hourOffset = 3;
+  date.setUTCHours(date.getUTCHours(), date.getUTCMinutes());
+  var time = date.getTime();
+  date.setUTCFullYear(date.getUTCFullYear(), 2, 22);
+  var dstStart = date.getTime();
+  date.setUTCFullYear(date.getUTCFullYear(), 8, 22);
+  var dstEnd = date.getTime();
+  if (time > dstStart && time < dstEnd) hourOffset = 4;
+  date.setUTCHours(date.getUTCHours() + hourOffset, date.getUTCMinutes() + 30);
+  var output =
+    date.getUTCHours() +
+    ":" +
+    date.getUTCMinutes() +
+    ":" +
+    date.getUTCSeconds();
+  document.getElementById("clock").innerHTML = output;
+}
+function setElement(currentUTCTime) {
+  // find id = time in html page and this is about content part
+  var dateTimeElement = document.getElementById("time");
 
-  // Get current date
-  var currentDate = new Date();
-  var options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  var formattedDate = currentDate.toLocaleDateString("en-US", options);
-  dateElement.textContent = formattedDate;
-
-  // Get current time
-  var hours = currentDate.getHours();
-  var minutes = currentDate.getMinutes();
-  var seconds = currentDate.getSeconds();
-  var formattedTime =
-    padZero(hours) + ":" + padZero(minutes) + ":" + padZero(seconds);
-  timeElement.textContent = formattedTime;
-});
-
-function padZero(num) {
-  return (num < 10 ? "0" : "") + num;
+  (dateTimeElement.textContent = currentUTCTime), clock();
 }
 
-console.log("This is a popup!");
+document.addEventListener("DOMContentLoaded", function () {
+  // set default value
+  const currentUTCTime = dateTime();
+  setElement(currentUTCTime);
+
+  // update default value each 1s
+  setInterval(() => {
+    const currentUTCTime = dateTime();
+    setElement(currentUTCTime);
+  }, 1000); // Run updateTime() every second
+});
